9d7e5b449692f8c4a023af1c43192598672ee8fd
Kiarash Hosseini <kia.hosseini7@gmail.com>
2017-05-11 22:46:12 +0430

    .

--- _includes/codes/pgadmin4_install/pgadmin4_install.sh
+++ _includes/codes/pgadmin4_install/pgadmin4_install.sh
@@ -1,2 +1,2 @@
-wget https://ftp.postgresql.org/pub/pgadmin3/pgadmin4/v1.0/pip/pgadmin4-1.0-py2-none-any.whl
-pip install pgadmin4-1.0-py2-none-any.whl
+wget https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v1.4/pip/pgadmin4-1.4-py2.py3-none-any.whl
+pip install pgadmin4-1.4-py2.py3-none-any.whl
