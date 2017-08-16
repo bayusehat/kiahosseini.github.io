69863d1a46d2f3af9e07e0ef80b5aa911f0fdc3e
kia7 <kia.hosseini7@gmail.com>
2017-08-16 18:55:00 +0430

    .

--- _includes/codes/pgadmin4_install/pgAdmin4.sh
+++ _includes/codes/pgadmin4_install/pgAdmin4.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 source ~/apps/pgadmin4/venv/bin/activate
 python ~/apps/pgadmin4/venv/local/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py & 
 sleep 5; sensible-browser http://127.0.0.1:5050
